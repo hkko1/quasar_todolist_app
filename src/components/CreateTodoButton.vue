66f29c489cfeca806d275f23e58579134d235042
hkko1 <hyekyung.ko@telus.com>
2023-02-10 06:30:11 -0800

    quasar todolist app first commit

---
+++ src/components/CreateTodoButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-btn @click="handleButtonClicked"/>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleButtonClicked () {
+      // alert('clicked')
+      this.$q.dialog({
+        title: 'Create Todo',
+        prompt: {
+          model: '',
+          type: 'text'
+        }
+      }).onOk(this.createTodo)
+    },
+
+    createTodo (data) {
+      // console.log(data)
+      this.$q.notify({
+        message: 'Todo Created',
+        icon: 'mdi-check',
+        color: 'positive'
+      })
+    }
+  }
+}
+</script>
